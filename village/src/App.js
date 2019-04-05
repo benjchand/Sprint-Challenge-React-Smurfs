537a7f6f3d990192c14461dad563b48416269d6a
benjchand <benchand@gmail.com>
2019-04-05 08:46:53 -0700

    Started.  API is imported.  Working on form.

--- village/src/App.js
+++ village/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
+import axios from 'axios';
+import { BrowserRouter as Router, Route, NavLink, withRouter } from 'react-router-dom';
+
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,6 +15,22 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount () {
+    console.log ("component has mounted");
+    axios
+      .get('http://localhost:3333/smurfs')
+        .then (res =>{
+          console.log(res);
+          this.setState({ smurfs: res.data })
+        })
+
+        .catch (err =>{
+          console.log(err);
+
+        })
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
